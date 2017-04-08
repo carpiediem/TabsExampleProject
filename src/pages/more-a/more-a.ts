51a5aa266db982a0d4e447c1b165938fed12b523
Ryan Carpenter <carpiediem@gmail.com>
2017-04-08 13:50:45 +0800

    Added back buttons to the three child pages of MoreTabs Browser back button doesn't do anything

--- src/pages/more-a/more-a.ts
+++ src/pages/more-a/more-a.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, App } from 'ionic-angular';
+import { TabsPage } from '../tabs/tabs';
 
 @Component({
   selector: 'page-more-a',
@@ -7,11 +8,17 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class MoreA {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private _app: App) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MoreA');
+  }
+
+  backToPrimaryTabs(): void {
+    //this.navCtrl.pop();  //Error: You can't remove all the pages in the navigation stack. nav.pop() is probably called too many times.
+    const root = this._app.getRootNav();
+    root.push(TabsPage);
   }
 
 }
